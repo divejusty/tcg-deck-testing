0598ca11ad15302b26d77a99cdf6d64556245121
Justin Wies <justin@wies.nl>
2023-05-01 07:18:56 +0200

    Started extracting the menu to a separate component

---
+++ resources/js/MenuEntry.mjs
@@ -0,0 +1,37 @@
+class MenuEntry {
+    constructor(name, target) {
+        this.name = name
+        
+        this.hasChildren = Array.isArray(target)
+        
+        if (this.hasChildren) {
+            this.route = undefined
+            this.children = target.map((item) => {
+                return new MenuEntry(item.name, item.target)
+            })
+        } else {
+            this.route = target
+            this.children = []
+        }
+    }
+    
+    getName() {
+        return this.name
+    }
+    
+    getRoute() {
+        return this.route
+    }
+    
+    getChildren() {
+        return this.children
+    }
+    
+    isLeaf() {
+        return !this.hasChildren
+    }
+}
+
+export {
+    MenuEntry
+}
